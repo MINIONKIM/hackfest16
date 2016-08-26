a4f9a5a3263eafa31353938f47f18d10c9f7c66f
Minhyun Kim <minionkim@outlook.com>
2016-08-27 00:32:50 +0900

    160826_minhkim

--- app/src/main/java/me/spheric/hackfest16/Connector.java
+++ app/src/main/java/me/spheric/hackfest16/Connector.java
@@ -3,5 +3,5 @@ package me.spheric.hackfest16;
 /**
  * Created by minion on 16. 8. 26..
  */
-public class ConnectorActivity {
+public class Connector {
 }
--- app/src/main/java/me/spheric/hackfest16/SplashActivity.java
+++ app/src/main/java/me/spheric/hackfest16/SplashActivity.java
@@ -1,13 +1,35 @@
 package me.spheric.hackfest16;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Handler hd = new Handler();
+        hd.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+
+            }
+        }, 2000);
+
+        Intent intent = null;
+        intent = new Intent(SplashActivity.this, MainActivity.class);
+
+        if (intent != null) {
+            SplashActivity.this.startActivity(intent);
+        }
+
+        finish();
+
     }
 }
